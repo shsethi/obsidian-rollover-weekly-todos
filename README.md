706f4ffc8016aa8d883d48ee30e997fb7858c9ee
shubham <5321007+shsethi@users.noreply.github.com>
2024-07-19 18:10:09 +0530

    first version release

--- README.md
+++ README.md
@@ -1,96 +1,41 @@
-# Obsidian Sample Plugin
+# Rollover Weekly Todos
 
-This is a sample plugin for Obsidian (https://obsidian.md).
 
-This project uses Typescript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in Typescript Definition format, which contains TSDoc comments describing what it does.
+This Obsidian plugin will rollover any incomplete todo items from the previous weekly note to current week's weekly note.
 
-**Note:** The Obsidian API is still in early alpha and is subject to change at any time!
+It's heavily inspired from the  [obsidian-rollover-daily-todos](https://github.dev/lumoe/obsidian-rollover-daily-todos/tree/master)
 
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open Sample Modal" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
+Since the existing plugin only allows rollover of daily notes, but for I prefer working with weekly notes as daily notes is too granular for my workflow.
 
-## First time developing plugins?
+Also this uses TypeScript instead of plain JS.
 
-Quick starting guide for new plugin devs:
+## Usage
+Open your command palette (CMD+P on macOS) and start typing roll to find this command. No matter where you are in Obsidian, the previous weeks's todos will get rolled forward.
 
-- Check if [someone already developed a plugin for what you want](https://obsidian.md/plugins)! There might be an existing plugin similar enough that you can partner up with.
-- Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
-- Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
-- Install NodeJS, then run `npm i` in the command line under your repo folder.
-- Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
-- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
-- Reload Obsidian to load the new version of your plugin.
-- Enable plugin in settings window.
-- For updates to the Obsidian API run `npm update` in the command line under your repo folder.
+Alternatively, you can use the icon in ribbon bar. 
 
-## Releasing new releases
+## Requirements
 
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments. Note: The manifest.json file must be in two places, first the root path of your repository and also in the release.
-- Publish the release.
+- [ ] You must have `Periodic Notes` plugin installed AND the **Weekly Notes** setting toggled on
+- [ ] A Note folder set in one of these plugins. Inside it you must have:
+  1. 2 or more notes
+  2. All notes must be named in the format you use for daily notes (for example `2021-08-29` for `YYYY-MM-DD` )
 
-> You can simplify the version bump process by running `npm version patch`, `npm version minor` or `npm version major` after updating `minAppVersion` manually in `manifest.json`.
-> The command will bump version in `manifest.json` and `package.json`, and add the entry for the new version to `versions.json`
 
-## Adding your plugin to the community plugin list
+## Settings
 
-- Check https://github.com/obsidianmd/obsidian-releases/blob/master/plugin-review.md
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
+### 1. Disable automatic rollover
+If you prefer to trigger the rollover of your todos manually, you can use this setting to prevent the plugin from rolling them over when a new note is created.
 
-## How to use
+### 2. Template Heading
+If you chose a template file to use for new daily notes in Daily notes > Settings or Periodic Notes > Settings, you will be able to choose a heading for incomplete notes to roll into. Note that incomplete todos are taken from the entire file, regardless of what heading they are under. And they are all rolled into today's daily note, right under the heading of choice.
 
-- Clone this repo.
-- Make sure your NodeJS is at least v16 (`node --version`).
-- `npm i` or `yarn` to install dependencies.
-- `npm run dev` to start compilation in watch mode.
+If you leave this field as blank, or select None, then incomplete todos will be rolled onto the end of today's note (for new notes with no template, the end is the beginning of the note).
 
-## Manually installing the plugin
+### 3. Delete todos from previous week
+By default, this plugin will actually make a copy of incomplete todos. So if you forgot to wash your dog yesterday, and didn't check it off, then you will have an incomplete checkmark on yesterday's daily note, and a new incomplete checkmark will be rolled into today's daily note. If you use the Undo last rollover command, deleted todos will be restored (remember, the time limit on this is 2 minutes).
 
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
+Toggling this setting on will remove incomplete todos from the previous daily note once today's daily note has a copy of them.
 
-## Improve code quality with eslint (optional)
-- [ESLint](https://eslint.org/) is a tool that analyzes your code to quickly find problems. You can run ESLint against your plugin to find common bugs and ways to improve your code. 
-- To use eslint with this project, make sure to install eslint from terminal:
-  - `npm install -g eslint`
-- To use eslint to analyze this project use this command:
-  - `eslint main.ts`
-  - eslint will then create a report with suggestions for code improvement by file and line number.
-- If your source code is in a folder, such as `src`, you can use eslint with this command to analyze all files in that folder:
-  - `eslint .\src\`
-
-## Funding URL
-
-You can include funding URLs where people who use your plugin can financially support it.
-
-The simple way is to set the `fundingUrl` field to your link in your `manifest.json` file:
-
-```json
-{
-    "fundingUrl": "https://buymeacoffee.com"
-}
-```
-
-If you have multiple URLs, you can also do:
-
-```json
-{
-    "fundingUrl": {
-        "Buy Me a Coffee": "https://buymeacoffee.com",
-        "GitHub Sponsor": "https://github.com/sponsors",
-        "Patreon": "https://www.patreon.com/"
-    }
-}
-```
-
-## API Documentation
-
-See https://github.com/obsidianmd/obsidian-api
+### 4. Remove empty todos in rollover
+By default, this plugin will roll over anything that has a checkbox, whether it has content or not. Toggling this setting on will ignore empty todos. If you have #2 from above toggled on, it will also delete empty todos.
--- main.ts
+++ main.ts
@@ -1,134 +1,294 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Notice, Plugin, TFile, moment } from "obsidian";
+import {
+	getAllWeeklyNotes,
+	getWeeklyNote,
+} from "obsidian-daily-notes-interface";
+// import moment from "moment";
+import { getTodos } from "./GetTodos";
+import UndoModal from "./UndoModal";
+import RolloverSettingTab from "./RolloverSettingTab";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface RolloverWeeklyTodosSettings {
+	templateHeading: string;
+	deleteOnComplete: boolean;
+	removeEmptyTodos: boolean;
+	rolloverChildren: boolean;
+	rolloverOnFileCreate: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: RolloverWeeklyTodosSettings = {
+	templateHeading: "none",
+	deleteOnComplete: false,
+	removeEmptyTodos: true,
+	rolloverChildren: true,
+	rolloverOnFileCreate: true,
+};
+
+// setup undo history
+interface UndoHistory {
+	previousDay: {
+		file: TFile;
+		oldContent: string;
+	};
+	today: {
+		file: TFile;
+		oldContent: string;
+	};
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+export default class RolloverWeeklyTodosPlugin extends Plugin {
+	settings: RolloverWeeklyTodosSettings;
+	undoHistoryTime: Date | null = null; // Initialize with null
+	undoHistory: UndoHistory[] = []; // Use array of UndoHistory
 
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
-}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+	async rollover(file?: TFile) {
+
+		if (!this.isWeeklyNotesEnabled()) {
+			new Notice(
+			  "RolloverTodosPlugin unable to rollover unfinished todos: Please enable Periodic Notes (with weekly notes enabled).",
+			  10000
+			);
+			return;
+		}
+
+	
+		const { templateHeading, deleteOnComplete, removeEmptyTodos } =
+			this.settings;
+
+		// const { moment } = window;
+		if (!file) {
+			const allWeeklyNotes = getAllWeeklyNotes();
+			file = getWeeklyNote(moment(), allWeeklyNotes); // use moment() directly
+		}
+
+		const previousWeekNote = this.getPreviousWeekNote(moment());
+		if (!previousWeekNote) {
+			new Notice("No previous week note found.");
+			return;
+		}
+
+		console.log("Current week note:", file.name);
+		console.log("Previous week note:", previousWeekNote.name);
+
+		// TODO: Implement your rollover logic here
+		// Read tasks from previousWeekNote and append to file
+
+		// get unfinished todos from yesterday, if exist
+		let todos_lastWeek = await this.getAllUnfinishedTodos(previousWeekNote);
+
+		console.log(
+			`rollover-daily-todos: ${todos_lastWeek.length} todos found in ${previousWeekNote.basename}.md`
+		);
+
+		if (todos_lastWeek.length == 0) {
+			return;
+		}
+
+		//   -----
+
+		let undoHistoryInstance: UndoHistory = {
+			previousDay: { file: previousWeekNote, oldContent: "" },
+			today: { file: file, oldContent: "" },
+		};
+
+		// Potentially filter todos from yesterday for today
+		let todosAdded = 0;
+		let emptiesToNotAddToTomorrow = 0;
+		let todos_today = !removeEmptyTodos ? todos_lastWeek : [];
+		if (removeEmptyTodos) {
+			todos_lastWeek.forEach((line, i) => {
+				const trimmedLine = (line || "").trim();
+				if (trimmedLine != "- [ ]" && trimmedLine != "- [  ]") {
+					todos_today.push(line);
+					todosAdded++;
+				} else {
+					emptiesToNotAddToTomorrow++;
+				}
+			});
+		} else {
+			todosAdded = todos_lastWeek.length;
+		}
+
+		// get today's content and modify it
+		let templateHeadingNotFoundMessage = "";
+		const templateHeadingSelected = templateHeading !== "none";
+
+		if (todos_today.length > 0) {
+			let dailyNoteContent = await this.app.vault.read(file);
+			undoHistoryInstance.today.oldContent = dailyNoteContent; // Update oldContent
+			const todos_todayString = `\n${todos_today.join("\n")}`;
+
+			// If template heading is selected, try to rollover to template heading
+			if (templateHeadingSelected) {
+				const contentAddedToHeading = dailyNoteContent.replace(
+					templateHeading,
+					`${templateHeading}${todos_todayString}`
+				);
+				if (contentAddedToHeading == dailyNoteContent) {
+					templateHeadingNotFoundMessage = `Rollover couldn't find '${templateHeading}' in today's daily not. Rolling todos to end of file.`;
+				} else {
+					dailyNoteContent = contentAddedToHeading;
+				}
+			}
+
+			// Rollover to bottom of file if no heading found in file, or no heading selected
+			if (
+				!templateHeadingSelected ||
+				templateHeadingNotFoundMessage.length > 0
+			) {
+				dailyNoteContent += todos_todayString;
+			}
+
+			await this.app.vault.modify(file, dailyNoteContent);
+		}
+
+		// if deleteOnComplete, get yesterday's content and modify it
+		if (deleteOnComplete) {
+			let previousWeekNoteContent = await this.app.vault.read(
+				previousWeekNote
+			);
+			undoHistoryInstance.previousDay = {
+				file: previousWeekNote,
+				oldContent: `${previousWeekNoteContent}`,
+			};
+			let lines = previousWeekNoteContent.split("\n");
+
+			for (let i = lines.length; i >= 0; i--) {
+				if (todos_lastWeek.includes(lines[i])) {
+					lines.splice(i, 1);
+				}
+			}
+
+			const modifiedContent = lines.join("\n");
+			await this.app.vault.modify(previousWeekNote, modifiedContent);
+		}
+
+		// Let user know rollover has been successful with X todos
+		const todosAddedString =
+			todosAdded == 0
+				? ""
+				: `- ${todosAdded} todo${
+						todosAdded > 1 ? "s" : ""
+				  } rolled over.`;
+		const emptiesToNotAddToTomorrowString =
+			emptiesToNotAddToTomorrow == 0
+				? ""
+				: deleteOnComplete
+				? `- ${emptiesToNotAddToTomorrow} empty todo${
+						emptiesToNotAddToTomorrow > 1 ? "s" : ""
+				  } removed.`
+				: "";
+		const part1 =
+			templateHeadingNotFoundMessage.length > 0
+				? `${templateHeadingNotFoundMessage}`
+				: "";
+		const part2 = `${todosAddedString}${
+			todosAddedString.length > 0 ? " " : ""
+		}`;
+		const part3 = `${emptiesToNotAddToTomorrowString}${
+			emptiesToNotAddToTomorrowString.length > 0 ? " " : ""
+		}`;
+
+		let allParts = [part1, part2, part3];
+		let nonBlankLines: string[] = [];
+		allParts.forEach((part) => {
+			if (part.length > 0) {
+				nonBlankLines.push(part);
+			}
+		});
+
+		const message = nonBlankLines.join("\n");
+		if (message.length > 0) {
+			new Notice(message, 4000 + message.length * 3);
+		}
+		this.undoHistoryTime = new Date();
+		this.undoHistory.push(undoHistoryInstance);
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	async getAllUnfinishedTodos(file: TFile) {
+		const dn = await this.app.vault.read(file);
+		const dnLines = dn.split(/\r?\n|\r|\n/g);
+
+		return getTodos({
+			lines: dnLines,
+			withChildren: this.settings.rolloverChildren,
+		});
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+	getPreviousWeekNote(date: moment.Moment) {
+		const previousWeekStart = date
+			.clone()
+			.subtract(1, "weeks")
+			.startOf("week");
+		return getWeeklyNote(previousWeekStart, getAllWeeklyNotes());
 	}
 
-	display(): void {
-		const {containerEl} = this;
+	async loadSettings() {
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
+	}
 
-		containerEl.empty();
+	isWeeklyNotesEnabled() {
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		// https://forum.obsidian.md/t/error-property-commands-does-not-exist-on-type-app/66924
+		//@ts-ignore
+		const periodicNotesPlugin = this.app.plugins.getPlugin("periodic-notes");
+
+		const periodicNotesEnabled =
+			periodicNotesPlugin && periodicNotesPlugin.settings?.weekly?.enabled;
+
+		return  periodicNotesEnabled;
+	}
+
+	async onload() {
+		console.log("Plugin loaded");
+		await this.loadSettings();
+
+		this.addSettingTab(new RolloverSettingTab(this.app, this));
+
+		// This creates an icon in the left ribbon.
+		const ribbonIconEl = this.addRibbonIcon(
+			"reset",
+			"RollOver Weekly Todos",
+			(evt: MouseEvent) => {
+				// Called when the user clicks the icon.
+				this.rollover();
+			}
+		);
+
+		this.addCommand({
+			id: "obsidian-rollover-weekly-todos-rollover",
+			name: "Rollover Weekly Todos Now",
+			callback: () => this.rollover(),
+		});
+
+		this.addCommand({
+			id: "obsidian-rollover-daily-todos-undo",
+			name: "Undo last rollover",
+			checkCallback: (checking) => {
+			  // no history, don't allow undo
+			  if (this.undoHistory.length > 0) {
+				const now = window.moment();
+				const lastUse = window.moment(this.undoHistoryTime);
+				const diff = now.diff(lastUse, "seconds");
+				// 2+ mins since use: don't allow undo
+				if (diff > 2 * 60) {
+				  return false;
+				}
+				if (!checking) {
+				  new UndoModal(this).open();
+				}
+				return true;
+			  }
+			  return false;
+			},
+		  });
 	}
 }
